30499a8704e1673f1430350aa112aaba112f3973
jawad-mohammed <mohammedjawad707@gmail.com>
2022-05-19 15:15:14 +0530

    1 file modified

---
+++ src/components/data.js
@@ -0,0 +1,47 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "Macbook",
+      price: 1400,
+      image:
+        "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_960_720.jpg"
+    },
+    {
+      id: 2,
+      name: "Car",
+      price: 5000,
+      image:
+        "https://cdn.pixabay.com/photo/2016/02/13/13/11/oldtimer-1197800_960_720.jpg"
+    },
+    {
+      id: 3,
+      name: "cycle",
+      price: 40,
+      image:
+        "https://cdn.pixabay.com/photo/2014/07/05/08/18/bicycle-384566_960_720.jpg"
+    },
+    {
+      id: 4,
+      name: "bottle",
+      price: 10,
+      image:
+        "https://cdn.pixabay.com/photo/2016/12/06/18/27/cereal-1887237_960_720.jpg"
+    },
+    {
+      id: 5,
+      name: "PC",
+      price: 2400,
+      image:
+        "https://cdn.pixabay.com/photo/2017/11/27/21/31/computer-2982270_960_720.jpg"
+    },
+    {
+      id: 6,
+      name: "shirt",
+      price: 15,
+      image:
+        "https://cdn.pixabay.com/photo/2016/03/25/09/04/t-shirt-1278404_960_720.jpg"
+    }
+  ]
+};
+export default data
